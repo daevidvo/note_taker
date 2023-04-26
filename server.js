3bd153008c0e0b8a4f08eb6af659839b24c137e3
daevidvo <itsdavidvo@gmail.com>
2023-04-25 20:22:30 -0700

    finished writing note taking functionality

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const fs = require('fs')
 const uniqid = require('uniqid')
 const notesData = require('./db/db.json')
 
-
 const app = exp();
 const PORT = 3001;
 
@@ -20,12 +19,15 @@ app.get('/notes', (req,res)=>{//path that requests the base URL plus /notes will
 })
 
 app.get('/api/notes', (req,res)=>{ //get request for when the saved notes want to be displayed
-    res.send(notesData)
+    fs.readFile('./db/db.json', 'utf-8', (err,data)=>{ //have to do fs.readFile or else it will just send the version of db.json that initially loaded when the server opened
+        if (err){
+            console.log(err);
+        }
+        res.send(data)
+    })
 })
 
 app.post('/api/notes', (req,res)=>{ //post request for when the save notes icon is used
-    console.log(req.body)
-
     const {title, text} = req.body;
 
     if(title && text){ //if both of the title and text are truthy values, then it will create the new object. If they aren't truthy values, then it will throw error
@@ -35,10 +37,18 @@ app.post('/api/notes', (req,res)=>{ //post request for when the save notes icon 
             id: uniqid('note-'), //id generator for the new notes
         };
 
-        notesData.push(newNote); //adds the new note to existing db.json object;
-        let newNotesData = JSON.stringify(notesData, null, 2) //stringifies the new notesData object with the new note
+        fs.readFile('./db/db.json', 'utf-8', (err, data)=>{
+            if(err){
+                console.log(err)
+            }else{
+                let parsedNotesData = JSON.parse(data)
 
-        fs.writeFile('./db/db.json', newNotesData, (err)=>err?console.log(err):console.log('db.json has been successfully updated')) //writes to the db.json file and updates it with the new notes
+                parsedNotesData.push(newNote)
+
+                fs.writeFile('./db/db.json', JSON.stringify(parsedNotesData, null, 2), (err)=>err?console.log(err):console.log('successfully added new note')) //writes to the db.json file and updates it with the new notes
+                res.send('new note creation successful') //NEEDED RES.SEND TO TELL THE FETCH REQUEST THAT THE FETCH HAS CONCLUDED SO THE .THEN FUNCTIONS CAN WORK
+            }
+        })
 
     }else{
         req.status(500).json('An error has occurred'); //tells the user if something went wrong while making the note
@@ -46,8 +56,8 @@ app.post('/api/notes', (req,res)=>{ //post request for when the save notes icon 
 });
 
 app.delete('/api/notes/:id', (req,res)=>{ //delete request for the note
-    console.log(req.method) //makes sure the request method is delete
-    console.log(req.params.id) //console logs the id that was used for :id. Use this when parsing through db.json to figure out which note to delete
+    // console.log(req.method) //makes sure the request method is delete
+    // console.log(req.params.id) //console logs the id that was used for :id. Use this when parsing through db.json to figure out which note to delete
     
     fs.readFile('./db/db.json', 'utf-8', (err, data)=>{ //reads the information from db.json where data = the stringified data from db.json
         if (err){
@@ -55,34 +65,18 @@ app.delete('/api/notes/:id', (req,res)=>{ //delete request for the note
         }else{
             let parsedNotesData = JSON.parse(data) //takes the stringified data from db.json and turns it back into an object
 
-            console.log(parsedNotesData) //verifies the existence of parsedNotesData
-
             for(let x=0;x<parsedNotesData.length;x=x+1){ //for loop that looks through the array in parsedNotesData and finds which element's id matches the one of the note's id that was requested to be deleted then deletes said note.
                 if (req.params.id === parsedNotesData[x].id){
                     parsedNotesData.splice(x, 1)
-                    console.log(parsedNotesData)
                 }
             }
 
             fs.writeFile('./db/db.json', JSON.stringify(parsedNotesData, null, 2), (err)=>err?console.log(err):console.log('note successfully deleted')) //updates the db.json file to represent the newly deleted data.
-            //for some reason the function to render the notes isn't working so that's weird :////////
+            res.send('note deletion successful') //NEEDED RES.SEND TO TELL THE FETCH REQUEST THAT THE FETCH HAS CONCLUDED SO THE .THEN FUNCTIONS CAN WORK
         }
     })
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(PORT, ()=>{
+app.listen(PORT, ()=>{ //initiates server.js on the specified port
     console.log(`Sever is open at http://localhost:${PORT}`)
 })
