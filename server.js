82fbf5e6872a892b993e80f75fe47efe622f2716
daevidvo <itsdavidvo@gmail.com>
2023-04-25 19:33:29 -0700

    added delete method functionality to server.js for when the user wants to delele a note

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const exp = require('express')
 const path = require('path')
 const fs = require('fs')
+const uniqid = require('uniqid')
 const notesData = require('./db/db.json')
 
 
@@ -23,9 +24,6 @@ app.get('/api/notes', (req,res)=>{ //get request for when the saved notes want t
 })
 
 app.post('/api/notes', (req,res)=>{ //post request for when the save notes icon is used
-    console.log(req.method);
-    console.log(notesData) //console.logs the note that the person just wrote
-    console.log(typeof notesData) //checks to see what data type the notesData is. Should be an object
     console.log(req.body)
 
     const {title, text} = req.body;
@@ -34,6 +32,7 @@ app.post('/api/notes', (req,res)=>{ //post request for when the save notes icon 
         const newNote = {
             title,
             text,
+            id: uniqid('note-'), //id generator for the new notes
         };
 
         notesData.push(newNote); //adds the new note to existing db.json object;
@@ -42,8 +41,33 @@ app.post('/api/notes', (req,res)=>{ //post request for when the save notes icon 
         fs.writeFile('./db/db.json', newNotesData, (err)=>err?console.log(err):console.log('db.json has been successfully updated')) //writes to the db.json file and updates it with the new notes
 
     }else{
-        req.status(500).json('An error has occurred');
+        req.status(500).json('An error has occurred'); //tells the user if something went wrong while making the note
     }
+});
+
+app.delete('/api/notes/:id', (req,res)=>{ //delete request for the note
+    console.log(req.method) //makes sure the request method is delete
+    console.log(req.params.id) //console logs the id that was used for :id. Use this when parsing through db.json to figure out which note to delete
+    
+    fs.readFile('./db/db.json', 'utf-8', (err, data)=>{ //reads the information from db.json where data = the stringified data from db.json
+        if (err){
+            console.log(err);
+        }else{
+            let parsedNotesData = JSON.parse(data) //takes the stringified data from db.json and turns it back into an object
+
+            console.log(parsedNotesData) //verifies the existence of parsedNotesData
+
+            for(let x=0;x<parsedNotesData.length;x=x+1){ //for loop that looks through the array in parsedNotesData and finds which element's id matches the one of the note's id that was requested to be deleted then deletes said note.
+                if (req.params.id === parsedNotesData[x].id){
+                    parsedNotesData.splice(x, 1)
+                    console.log(parsedNotesData)
+                }
+            }
+
+            fs.writeFile('./db/db.json', JSON.stringify(parsedNotesData, null, 2), (err)=>err?console.log(err):console.log('note successfully deleted')) //updates the db.json file to represent the newly deleted data.
+            //for some reason the function to render the notes isn't working so that's weird :////////
+        }
+    })
 })
 
 
